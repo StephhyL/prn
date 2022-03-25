06af1398ee4c27c5629a7ced4d52fe2713352c2b
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-03-25 14:59:28 -0400

    inital commit for Timer

---
+++ src/components/timer/Timer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// import styling
+import "../../stylesheets/css/Timer.css";
+
+const Timer = () => {
+  return <div>Hello from Timer</div>;
+};
+
+export default Timer;
