b574c075ba7f8342993eadff5f15ebe8cadbfa60
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-03-25 15:04:12 -0400

    import Timer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // import other Components
 import Home from "./components/home/Home";
+import Timer from "./components/timer/Timer";
 
 // import styling
 import "./stylesheets/css/App.css";
@@ -15,6 +16,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        <Timer />
       </Router>
     </div>
   );
