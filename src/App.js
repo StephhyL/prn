06ffa6f9ae5c30a489265b9b00fb72be0d3e3f42
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-03-25 14:45:52 -0400

    add routes with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+// import from other libraries
+import { BroswerRouter as Router, Routes, Route } from "react-router-dom";
+
+// import other Components
+
+// import styling
 import "./App.css";
 
 const App = () => {
-  return <div className="App">Hello from App</div>;
+  return (
+    <div className="App">
+      Hello from App
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
