3fe3466de6b87c13124f53ab628070696ef7bc48
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-03-25 19:27:39 -0400

    home layout

--- src/components/home/Category.jsx
+++ src/components/home/Category.jsx
@@ -1,8 +1,26 @@
 // import styling
 import "../../stylesheets/css/Category.css";
 
-const Category = () => {
-  return <div className="category">Hello from One Category</div>;
+const Category = (props) => {
+  const { catImg } = props;
+
+  // console.log("catImg --> ", catImg);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="category" onClick={handleClick}>
+      <img
+        src={catImg}
+        className="logo"
+        alt="prn-logo"
+        width="125"
+        // height="200"
+      />
+    </div>
+  );
 };
 
 export default Category;
