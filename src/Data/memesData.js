bfe9294a97c8341b5bcc681d267cf7802c3fd447
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-03-26 15:14:38 -0400

    add memes

--- src/Data/memesData.js
+++ src/Data/memesData.js
@@ -1,32 +1,21 @@
-const memes = [
+import meme1 from "./memeImg/meme1.png";
+import meme2 from "./memeImg/meme2.jpeg";
+import meme3 from "./memeImg/meme3.jpeg";
+import meme4 from "./memeImg/meme4.png";
+
+const memesData = [
   {
-    meme: "https://i.redd.it/ktsjyijdkfj61.jpg",
+    meme: meme1,
   },
   {
-    meme: "https://i.redd.it/5lg0ayq6lhj61.jpg",
+    meme: meme2,
   },
   {
-    meme: "https://i.redd.it/ncc0xq5sfjj61.jpg",
+    meme: meme3,
   },
   {
-    meme: "https://i.redd.it/q5g8mmhdzcj61.png",
-  },
-  {
-    meme: "https://i.redd.it/bv9e6vu8jgj61.jpg",
-  },
-  {
-    meme: "https://i.redd.it/akw575wrfgj61.gif",
-  },
-  {
-    meme: "https://i.redd.it/w3fnpu2e7jj61.jpg",
-  },
-  {
-    meme: "https://i.redd.it/hzlxved5uij61.jpg",
-  },
-  {
-    meme: "https://i.redd.it/u6ymwmbtahj61.jpg",
-  },
-  {
-    meme: "https://i.redd.it/x0uovypdihj61.jpg",
+    meme: meme4,
   },
 ];
+
+export { memesData };
--- src/components/categories/Memes.jsx
+++ src/components/categories/Memes.jsx
@@ -1,22 +1,75 @@
-// import other Components
-import Back from "./Back";
-
-// import data
-import { memesData } from "../../Data/memesData";
-
-// import stylesheet
 import "../../stylesheets/css/Memes.css";
+import { useState } from "react";
+import { memesData } from "../../Data/memesData";
+import Button from "@mui/material/Button";
+import ArrowBackSharpIcon from "@mui/icons-material/ArrowBackSharp";
+import ArrowForwardSharpIcon from "@mui/icons-material/ArrowForwardSharp";
+import Card from "../../UI/Card";
+import Back from "./Back";
+import { blue } from "@mui/material/colors";
 
-const Memes = () => {
-  console.log("memesData--->", memesData);
+function Memes() {
+  const [currentJoke, setCurrentJoke] = useState(0);
+
+  console.log("dadJokesData--->", memesData);
+
+  const onLeftClick = () => {
+    if (currentJoke > 0 && setCurrentJoke(currentJoke - 1)) {
+    } else if (currentJoke === 0) {
+      setCurrentJoke(memesData.length - 1);
+    }
+  };
+
+  const onRightClick = () => {
+    if (currentJoke === memesData.length - 1) {
+      setCurrentJoke(0);
+    } else {
+      setCurrentJoke(currentJoke + 1);
+    }
+  };
+
   return (
-    <div>
-      <div className="back">
-        <Back />
+    <div className="meme">
+      <div className="backArrow">
+        <div className="back">
+          <Back />
+        </div>
       </div>
-      Memes
+      <Card className="meme-joke">
+        <img src={memesData[currentJoke].meme} width="500" />
+
+        {/* <h1 className="joke-text">{memesData[currentJoke].meme}</h1> */}
+      </Card>
+      <div className="arrow-container">
+        <div className="left-click">
+          <Button
+            className="backNext"
+            onClick={() => onLeftClick()}
+            color="primary"
+            // variant="variant"
+            sx={{ color: blue[500] }}
+            aria-label="add"
+          >
+            <ArrowBackSharpIcon classname="arrow-icon large" />
+          </Button>
+        </div>
+
+        <div className="right-click">
+          <Button
+            className="backNext"
+            onClick={() => onRightClick()}
+            // variant="variant"
+            size="large"
+            // sx={{ boxShadow: 3 }}
+            color="primary"
+            aria-label="add"
+          >
+            <ArrowForwardSharpIcon classname="arrow-icon" />
+          </Button>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default Memes;
