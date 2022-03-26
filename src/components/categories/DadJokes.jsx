a908658d5aa71750c969c54eb38558d1cef10d0b
Habon <habonhassan01@gmail.com>
2022-03-26 12:05:09 -0400

    Add buttons with MUI

--- src/components/categories/DadJokes.jsx
+++ src/components/categories/DadJokes.jsx
@@ -3,8 +3,9 @@ import { useState } from "react";
 import { dadJokesData } from "../../Data/dadJokesData";
 import ArrowBackIosSharpIcon from "@mui/icons-material/ArrowBackIosSharp";
 import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+import Button from '@mui/material/Button';
 import Card from "../../UI/Card";
-
+// import Button from ""
 function DadJokes() {
   const [currentJoke, setCurrentJoke] = useState(0);
 
@@ -32,19 +33,22 @@ function DadJokes() {
         
       </Card>
       <div className="arrow-container">
-        <Card className="left-click" onClick={() => onLeftClick()}>
-          <div >
-            <ArrowBackIosSharpIcon style={{ fontSize: 30 }} />
+          <div className="left-click" onClick={() => onLeftClick()}>
+        <Button variant="contained" size="large" endIcon={<ArrowBackIosSharpIcon/>}>
+            {/* <ArrowBackIosSharpIcon style={{ fontSize: 30 }} /> */}
+            </Button>
           </div>
-        </Card>
-        <Card className="right-click" onClick={() => onRightClick()}>
+
+          <div className="right-click" onClick={() => onRightClick()}>
+        <Button variant="contained" endIcon={<ArrowForwardIosSharpIcon />}/>
     
-            <ArrowForwardIosSharpIcon style={{ fontSize: 30 }} />
-          
-        </Card>
+        
+          </div>
+        
       </div>
     </div>
   );
 }
 
 export default DadJokes;
+
