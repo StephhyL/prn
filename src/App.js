3d7e1f0eb109fe70c1f234a1542f85688b019f30
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-03-25 21:00:47 -0400

    merge conflict

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import DadJokes from "./components/categories/DadJokes";
 import Timer from "./components/timer/Timer";
 import DadJokes from "./components/categories/DadJokes";
 import DarkJokes from "./components/categories/DarkJokes";
-import Guess from "./components/categories/Guess"
-import Laugh from "./components/categories/Laugh"
+import Guess from "./components/categories/Guess";
+import Laugh from "./components/categories/Laugh";
 import Memes from "./components/categories/Memes";
 
 // import styling
@@ -19,18 +19,8 @@ const App = () => {
     <div className="App">
       <Router>
         <Routes>
-<<<<<<< HEAD
-          <Route path="/" element={<Home />}/>
-          <Route path="/dadjokes" element={<DadJokes />} />
-          <Route path="/darkjokes" element={<DarkJokes />} />
-          <Route path="/guess" element={<Guess />} />
-          <Route path="/laugh" element={<Laugh />} />
-          <Route path="/memes" element={<Memes />} />
-          <Route path="/*" element={<Home />}/>
-=======
           <Route path="/" element={<Home />} />
           <Route path="/DadJokes" element={<DadJokes />} />
->>>>>>> 16b9dfaf6e5971c9b1d736d6748750c2232f717b
         </Routes>
         <Timer />
       </Router>
