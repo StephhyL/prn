351b270c8daed01a6f5071de823ff8e9e6a535f3
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-03-25 21:54:18 -0400

    merge

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import other Components
 import Home from "./components/home/Home";
 import Timer from "./components/timer/Timer";
+import DadJokes from "./components/categories/DadJokes";
 import DarkJokes from "./components/categories/DarkJokes";
-import Guess from "./components/categories/Guess"
-import Laugh from "./components/categories/Laugh"
+import Guess from "./components/categories/Guess";
+import Laugh from "./components/categories/Laugh";
 import Memes from "./components/categories/Memes";
 
 // import styling
@@ -17,13 +18,13 @@ const App = () => {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />}/>
+          <Route path="/" element={<Home />} />
           <Route path="/dadjokes" element={<DadJokes />} />
           <Route path="/darkjokes" element={<DarkJokes />} />
           <Route path="/guess" element={<Guess />} />
           <Route path="/laugh" element={<Laugh />} />
           <Route path="/memes" element={<Memes />} />
-          <Route path="/*" element={<Home />}/>
+          <Route path="/*" element={<Home />} />
         </Routes>
         <Timer />
       </Router>
