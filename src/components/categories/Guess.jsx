efcf7dafb05a0020ecedec769c59fdc8c061b708
mcnlin <mcnghiem@gmail.com>
2022-03-26 01:06:05 -0400

    add bootstrap react, styling guess file

--- src/components/categories/Guess.jsx
+++ src/components/categories/Guess.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { InputGroup, FormControl, Button} from 'react-bootstrap'
 
+import "../../stylesheets/scss/Guess.scss"
 const Guess = () => {
   return (
-    <div>Guess</div>
+    <div className='guess'>
+    <div className='question'></div>
+    <InputGroup className="mb-3">
+    <FormControl
+      placeholder="Take a guess!"
+      aria-describedby="basic-addon2"
+    />
+    <Button className='enterButton' variant="outline-secondary" id="button-addon2">
+      Enter
+    </Button>
+  </InputGroup>
+    </div>
   )
 }
 
