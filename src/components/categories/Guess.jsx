c40a1a40e940afbefe979370fd9f31ca97c17ab7
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-03-26 12:12:43 -0400

    className

--- src/components/categories/Guess.jsx
+++ src/components/categories/Guess.jsx
@@ -10,15 +10,13 @@ import { guess } from "../../Data/guessData.js";
 import "../../stylesheets/scss/Guess.scss";
 
 const Guess = () => {
-  
   const [currentJoke, setCurrentJoke] = useState(0);
   const [question, setQuestion] = useState(true);
 
-
   console.log("guessData-->", guess);
   const onClick = () => {
-    setQuestion(!question)
-  }
+    setQuestion(!question);
+  };
   const onLeftClick = () => {
     if (currentJoke > 0 && setCurrentJoke(currentJoke - 1)) {
     } else if (currentJoke === 0) {
@@ -39,22 +37,20 @@ const Guess = () => {
       <div className="back">
         <Back />
       </div>
-      <div class="flip-card">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" onClick={onClick}>
-       
-            <p className="answer" >{question === true ? guess[currentJoke].question :guess[currentJoke].answer}</p> 
-            </div>
-          {/* <div class="flip-card-back"> */}
-        
-           
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front" onClick={onClick}>
+            <p className="answer">
+              {question === true
+                ? guess[currentJoke].question
+                : guess[currentJoke].answer}
+            </p>
           </div>
+          {/* <div class="flip-card-back"> */}
         </div>
-  
-      
+      </div>
     </>
   );
 };
 
 export default Guess;
-
