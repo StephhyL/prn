d76c7470dffd57b66b46ed5934ee5dfa5db90e7f
mcnlin <mcnghiem@gmail.com>
2022-03-25 20:52:30 -0400

    add categories routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import other Components
 import Home from "./components/home/Home";
 import Timer from "./components/timer/Timer";
+import DadJokes from "./components/categories/DadJokes";
+import DarkJokes from "./components/categories/DarkJokes";
+import Guess from "./components/categories/Guess"
+import Laugh from "./components/categories/Laugh"
+import Memes from "./components/categories/Memes";
 
 // import styling
 import "./stylesheets/css/App.css";
@@ -13,7 +18,13 @@ const App = () => {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />}/>
+          <Route path="/dadjokes" element={<DadJokes />} />
+          <Route path="/darkjokes" element={<DarkJokes />} />
+          <Route path="/guess" element={<Guess />} />
+          <Route path="/laugh" element={<Laugh />} />
+          <Route path="/memes" element={<Memes />} />
+          <Route path="/*" element={<Home />}/>
         </Routes>
         <Timer />
       </Router>
