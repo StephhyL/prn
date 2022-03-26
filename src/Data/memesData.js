5163da0a779cba6eefd31cf451fbc43c1629722c
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-03-26 14:13:01 -0400

    meme data

---
+++ src/Data/memesData.js
@@ -0,0 +1,32 @@
+const memes = [
+  {
+    meme: "https://i.redd.it/ktsjyijdkfj61.jpg",
+  },
+  {
+    meme: "https://i.redd.it/5lg0ayq6lhj61.jpg",
+  },
+  {
+    meme: "https://i.redd.it/ncc0xq5sfjj61.jpg",
+  },
+  {
+    meme: "https://i.redd.it/q5g8mmhdzcj61.png",
+  },
+  {
+    meme: "https://i.redd.it/bv9e6vu8jgj61.jpg",
+  },
+  {
+    meme: "https://i.redd.it/akw575wrfgj61.gif",
+  },
+  {
+    meme: "https://i.redd.it/w3fnpu2e7jj61.jpg",
+  },
+  {
+    meme: "https://i.redd.it/hzlxved5uij61.jpg",
+  },
+  {
+    meme: "https://i.redd.it/u6ymwmbtahj61.jpg",
+  },
+  {
+    meme: "https://i.redd.it/x0uovypdihj61.jpg",
+  },
+];
