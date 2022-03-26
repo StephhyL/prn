3697be7bc21761c7662676695e58a48be20b4898
Xihai Luo <xihai.luo01@gmail.com>
2022-03-26 01:31:15 -0400

    implement alarm sound for timer

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { timeFormat } from "./helper/timeFormat";
+import useSound from "use-sound";
 
 // import styling
 import "../../stylesheets/css/Timer.css";
+// import mp3 audio for timer
+import timerSound from "./alarm.mp3";
 
 const Timer = (props) => {
   const { timer } = props;
   const [counter, setCounter] = useState(timer);
+  const [playSound, setPlaySound] = useState(false);
+  const [play] = useSound(timerSound);
+
   console.log(counter);
 
   useEffect(() => {
@@ -15,6 +21,9 @@ const Timer = (props) => {
   }, [timer]);
 
   useEffect(() => {
+    if (counter === 0) {
+      setPlaySound(true);
+    }
     const timer =
       counter > 0 &&
       setInterval(() => {
@@ -22,6 +31,13 @@ const Timer = (props) => {
       }, 1000);
     return () => clearInterval(timer);
   }, [counter]);
+
+  useEffect(() => {
+    // play sound
+    play();
+    // set play sound to false
+    setPlaySound(false);
+  }, [playSound]);
 
   return (
     <div className="timer">
